63ff1f63ad04e032edabcb7ab97e553225f75bbd
Vraj Patel <patelvrajn@gmail.com>
2026-05-07 18:02:40 -0400

    Introduce Psuedo RNG class from Stockfish and make Zobrist Hash Keys use a constexpr LUT and make Zobrist Key functions constexpr

---
+++ include/psuedo_random_number_generator.hpp
@@ -0,0 +1,66 @@
+#pragma once
+
+#include <cstdint>
+
+// xorshift64star Pseudo-Random Number Generator
+// This class is based on original code written and dedicated to the public
+// domain by Sebastiano Vigna (2014).
+//
+// It has the following characteristics:
+//
+//  -  Outputs 64-bit numbers
+//  -  Passes Dieharder and SmallCrush test batteries
+//  -  Does not require warm-up, no zeroland to escape
+//  -  Internal state is a single 64-bit integer
+//  -  Period is 2^64 - 1
+//
+// For further analysis see
+//   <http://vigna.di.unimi.it/ftp/papers/xorshift.pdf>
+
+constexpr uint64_t GLOBAL_PRNG_DEFAULT_SEED = 1070372; // Seed from Stockfish
+
+template <typename T>
+class Psuedo_RNG
+{
+
+  public:
+
+    constexpr Psuedo_RNG(uint64_t seed = GLOBAL_PRNG_DEFAULT_SEED);
+
+    constexpr T generate_random();
+    constexpr T generate_sparse_random();
+
+  private:
+
+    uint64_t m_seed;
+
+    constexpr uint64_t generate_random_64();
+};
+
+template <typename T>
+constexpr Psuedo_RNG<T>::Psuedo_RNG(uint64_t seed) : m_seed(seed)
+{
+}
+
+template <typename T>
+constexpr uint64_t Psuedo_RNG<T>::generate_random_64()
+{
+    m_seed ^= m_seed >> 12;
+    m_seed ^= m_seed << 25;
+    m_seed ^= m_seed >> 27;
+
+    return m_seed * 2685821657736338717LL;
+}
+
+template <typename T>
+constexpr T Psuedo_RNG<T>::generate_random()
+{
+    return static_cast<T>(generate_random_64());
+}
+
+template <typename T>
+constexpr T Psuedo_RNG<T>::generate_sparse_random()
+{
+    return static_cast<T>(generate_random_64() & generate_random_64()
+                          & generate_random_64());
+}
--- include/zobrist_hash.hpp
+++ include/zobrist_hash.hpp
@@ -4,6 +4,7 @@
 #include <cstdint>
 
 #include "globals.hpp"
+#include "psuedo_random_number_generator.hpp"
 #include "square.hpp"
 
 struct Zobrist_Hash_Keys
@@ -17,33 +18,95 @@ struct Zobrist_Hash_Keys
     uint64_t                                                  black_to_move;
 };
 
+consteval Zobrist_Hash_Keys initialize_hash_keys()
+{
+    Zobrist_Hash_Keys    keys;
+    Psuedo_RNG<uint64_t> prng;
+
+    for (uint8_t player = 0; player < NUM_OF_PLAYERS; player++)
+    {
+        for (uint8_t piece = 0; piece < NUM_OF_UNIQUE_PIECES_PER_PLAYER;
+             piece++)
+        {
+            for (uint8_t square = 0; square < NUM_OF_SQUARES_ON_CHESS_BOARD;
+                 square++)
+            {
+                keys.pieces[player][piece][square] = prng.generate_random();
+            }
+        }
+    }
+
+    for (uint8_t square = 0; square < (NUM_OF_SQUARES_ON_CHESS_BOARD + 1);
+         square++)
+    {
+        keys.en_passant[square] = prng.generate_random();
+    }
+
+    for (uint8_t cr = 0; cr < 16; cr++)
+    {
+        keys.castling_rights[cr] = prng.generate_random();
+    }
+
+    keys.black_to_move = prng.generate_random();
+
+    return keys;
+}
+
 class Zobrist_Hash
 {
   public:
 
-    Zobrist_Hash();
+    constexpr Zobrist_Hash();
 
-    uint64_t get_hash_value() const;
+    constexpr uint64_t get_hash_value() const;
 
-    void update_piece(const PIECE_COLOR color,
-                      const PIECES      piece,
-                      const Square      square);
-    void update_en_passant_square(const Square s);
-    void update_castling_rights(const uint8_t new_rights);
-    void flip_side_to_move();
+    constexpr void update_piece(const PIECE_COLOR color,
+                                const PIECES      piece,
+                                const Square      square);
+    constexpr void update_en_passant_square(const Square s);
+    constexpr void update_castling_rights(const uint8_t new_rights);
+    constexpr void flip_side_to_move();
 
-    inline auto operator<=>(const Zobrist_Hash& other) const;
+    constexpr inline auto operator<=>(const Zobrist_Hash& other) const;
 
   private:
 
     uint64_t m_hash_value;
 
-    static Zobrist_Hash_Keys m_hash_keys;
-
-    static Zobrist_Hash_Keys initialize_hash_keys();
+    inline static constexpr Zobrist_Hash_Keys m_hash_keys =
+        initialize_hash_keys();
 };
 
-inline auto Zobrist_Hash::operator<=>(const Zobrist_Hash& other) const
+constexpr inline auto Zobrist_Hash::operator<=>(const Zobrist_Hash& other) const
 {
     return (get_hash_value() <=> other.get_hash_value());
+}
+
+constexpr Zobrist_Hash::Zobrist_Hash() : m_hash_value(0) {}
+
+constexpr uint64_t Zobrist_Hash::get_hash_value() const
+{
+    return m_hash_value;
+};
+
+constexpr void Zobrist_Hash::update_piece(const PIECE_COLOR color,
+                                          const PIECES      piece,
+                                          const Square      square)
+{
+    m_hash_value ^= m_hash_keys.pieces[color][piece][square.get_index()];
+}
+
+constexpr void Zobrist_Hash::update_en_passant_square(const Square s)
+{
+    m_hash_value ^= m_hash_keys.en_passant[s.get_index()];
+}
+
+constexpr void Zobrist_Hash::update_castling_rights(const uint8_t new_rights)
+{
+    m_hash_value ^= m_hash_keys.castling_rights[new_rights];
+}
+
+constexpr void Zobrist_Hash::flip_side_to_move()
+{
+    m_hash_value ^= m_hash_keys.black_to_move;
 }
