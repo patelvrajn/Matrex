e233de102b5852951ca400e59c157164f2f657f8
Vraj Patel <patelvrajn@gmail.com>
2026-05-09 16:56:28 -0400

    Make bitboard and square constexpr-able.

--- include/square.hpp
+++ include/square.hpp
@@ -85,30 +85,78 @@ class Square
 {
   public:
 
-    Square();
-    Square(uint8_t index);
-    Square(uint8_t r, uint8_t f);
+    constexpr Square();
+    constexpr Square(uint8_t index);
+    constexpr Square(uint8_t r, uint8_t f);
 
-    bool is_light_square() const;
-    bool is_dark_square() const;
+    constexpr bool is_light_square() const;
+    constexpr bool is_dark_square() const;
 
-    uint8_t  get_index() const;
-    uint8_t  get_rank() const;
-    uint8_t  get_file() const;
-    uint64_t get_mask() const;
-    uint8_t  get_diagonal() const;
-    uint8_t  get_antidiagonal() const;
+    constexpr uint8_t  get_index() const;
+    constexpr uint8_t  get_rank() const;
+    constexpr uint8_t  get_file() const;
+    constexpr uint64_t get_mask() const;
+    constexpr uint8_t  get_diagonal() const;
+    constexpr uint8_t  get_antidiagonal() const;
 
-    bool operator==(const Square& other) const;
+    constexpr bool operator==(const Square& other) const;
 
   private:
 
     uint8_t m_index;
 };
 
-inline uint64_t Square::get_mask() const
+constexpr Square::Square() : m_index(0) {}
+
+constexpr Square::Square(uint8_t index) : m_index(index) {}
+
+constexpr Square::Square(uint8_t r, uint8_t f)
+{
+    m_index = (r << 3) + f; // (8 * rank) + file
+}
+
+constexpr bool Square::is_light_square() const
+{
+    // A square is light if the sum of its rank and file is even.
+    // We compute parity using bitwise AND with 1 instead of modulo (% 2),
+    // because (x & 1) is faster than (x % 2).
+    return (((get_rank() + get_file()) & 1) == 0);
+}
+
+constexpr bool Square::is_dark_square() const { return (!is_light_square()); }
+
+constexpr uint8_t Square::get_index() const { return m_index; }
+
+constexpr uint8_t Square::get_rank() const
+{
+    return (m_index >> 3); // Divide by 8
+}
+
+constexpr uint8_t Square::get_file() const
+{
+    return (m_index & 7); // Modulo 8
+}
+
+constexpr uint64_t Square::get_mask() const
 { // Does not return a bitboard otherwise,
   // there is a circular dependency.
     uint64_t temp = 1;
     return (temp << m_index);
+}
+
+constexpr uint8_t Square::get_diagonal() const
+{
+    return 7
+         + (this->get_rank()
+            - this->get_file()); // 7 is to normalize (-7 to 7) to (0 to 14)
+}
+
+constexpr uint8_t Square::get_antidiagonal() const
+{
+    return (this->get_rank() + this->get_file());
+}
+
+constexpr bool Square::operator==(const Square& other) const
+{
+    return (this->m_index == other.m_index);
 }
