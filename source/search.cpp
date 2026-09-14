277a38002bffe5f9a6c4732a8105968c302c06fc
Vraj Patel <patelvrajn@gmail.com>
2026-09-14 11:23:40 -0400

    LMR comment.

--- source/search.cpp
+++ source/search.cpp
@@ -341,6 +341,11 @@ Search_Engine::negamax(Chess_Board&                    position,
                     static_cast<Depth_Int>(1),
                     static_cast<Depth_Int>(4));
 
+                // Late move reductions (LMR) - search this subtree with reduced 
+                // depth because we are late(r) into the move ordering and we 
+                // assume that these moves are not as good as the earlier moves.
+                // Note, we use move score as indicator of move quality rather 
+                // than move index in the condition to do LMR.
                 child_result = negamax(position,
                                        (depth - 1 - depth_reduction),
                                        leaf_nodes_welford,
